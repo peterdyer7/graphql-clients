655e87677070ef4717e798c8e5c2a10e3efbfb06
peterdyer7 <peter_dyer@hotmail.com>
2019-03-16 15:32:13 -0400

    UrqlTodo wired and working

--- src/UrqlTodo/UrqlTodo.jsx
+++ src/UrqlTodo/UrqlTodo.jsx
@@ -1,10 +1,71 @@
 import React from 'react';
-import { Header } from 'semantic-ui-react';
+import {
+  Container,
+  Segment,
+  Grid,
+  Dimmer,
+  Loader,
+  Header
+} from 'semantic-ui-react';
+import { Provider, createClient, Query, Mutation } from 'urql';
+import gql from 'graphql-tag';
 
-export default function Urql() {
+import ListTodos from '../ListTodos/ListTodos';
+
+import aws_config from '../aws-exports';
+import * as queries from '../graphql/queries';
+import * as mutations from '../graphql/mutations';
+import AddTodo from '../AddTodo/AddTodo';
+
+const client = createClient({
+  url: aws_config.aws_appsync_graphqlEndpoint,
+  fetchOptions: {
+    headers: {
+      'Content-Type': 'application/json',
+      'x-api-key': aws_config.aws_appsync_apiKey
+    }
+  }
+});
+
+export default function UrqlTodoWithProvider() {
   return (
-    <>
-      <Header as="h1">Urql</Header>
-    </>
+    <Provider value={client}>
+      <UrqlTodo />
+    </Provider>
+  );
+}
+
+function UrqlTodo() {
+  return (
+    <Container>
+      <Grid stackable columns={2}>
+        <Grid.Column>
+          <Segment>
+            <Query query={gql(queries.listTodos)} variables={{ limit: 10 }}>
+              {({ fetching, data, error }) => {
+                if (error)
+                  return <Header as="h4">Error: {error.message}</Header>;
+                if (fetching)
+                  return (
+                    <Dimmer active>
+                      <Loader />
+                    </Dimmer>
+                  );
+                if (!data || !data.listTodos || !data.listTodos.items)
+                  return <Header as="h4">No todos</Header>;
+                return <ListTodos todos={data.listTodos.items} />;
+              }}
+            </Query>
+          </Segment>
+        </Grid.Column>
+        <Grid.Column>
+          <Segment>
+            <Mutation query={gql(mutations.createTodo)}>
+              {({ executeMutation }) => <AddTodo addTodo={executeMutation} />}
+            </Mutation>
+          </Segment>
+        </Grid.Column>
+      </Grid>
+    </Container>
   );
 }
